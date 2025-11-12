3320fd5d5692c95f7ef4787f47f4a0f8e695f49a
DiMiRka <fallen_94@bk.ru>
2025-11-12 15:41:23 +0700

    Добавили конфигурацию

---
+++ src/core/__init__.py
@@ -0,0 +1,8 @@
+from src.core.config import app_settings, uvicorn_options
+from src.core.db_config import db_dependency
+
+__all__ = [
+    "app_settings",
+    "uvicorn_options",
+    "db_dependency"
+]
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
